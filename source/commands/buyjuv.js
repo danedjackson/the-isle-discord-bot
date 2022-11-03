2223532d627c88c6525368211d3f87d490b27be4
Dane <danedjackson@yahoo.com>
2022-11-03 09:11:00 -0500

    Adding free juvinile grow command

---
+++ source/commands/buyjuv.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const config = require('../cfg/config.json');
+const User = require('../models/user');
+const DinoInfo = require('../models/dinoInfo');
+
+const { queueHandler } = require("../functions/handlers/queue-handler");
+
+exports.run = async (client, message, args) =>{
+    
+    //If direct command with parameters used :
+    if (args.length != 0) {
+        var requestedDinoName = args[0];
+        var isSafelogged = args[1];
+        var steamId = args[2];
+
+        //Check if steamId is numeric
+
+
+        //Checks if safelog flag starts with y
+        if(!isSafelogged.toLowerCase().startsWith('y')) {
+            message.reply(`you must be safelogged before requesting a dinosaur.`);
+            return;
+        }
+
+        //Check if requested dino name is valid
+        try{
+            await mongoose.connect(config.mongodb.uri);
+            
+            var dinoInfo = await DinoInfo.find( {codeName: requestedDinoName.toLowerCase()} );
+            if(dinoInfo.length < 1) {
+                message.reply(`Incorrect dino name entered, please try again.`);
+                return;
+            }
+        } catch (err) {
+            console.error(`${message.author.username} | something went wrong connecting to mongo DB:\n${err}`);
+            message.reply(`Something went wrong on the server. Please try again later.`);
+            return;
+        } 
+        //Format code name for dinosaur to grow
+        var dinoName = dinoInfo[0].survival ? dinoInfo[0].codeName + "JuvS" : dinoInfo[0].codeName + "Juv";
+        //Capitalizing first letter of dinosaur name for the JSON filename
+        dinoName = dinoName.charAt(0).toUpperCase() + dinoName.slice(1);
+
+        await queueHandler( ["grow", dinoName, steamId, message] );
+    }
+}
